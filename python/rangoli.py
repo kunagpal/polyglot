3bd18283fd8d3bd51d858a24d09d8f4fcc101819
kunagpal <kunagpal@gmail.com>
2016-05-09 09:33:47 +0530

    Pattern cretaion reduction

--- python/rangoli.py
+++ python/rangoli.py
@@ -1,5 +1,4 @@
-from math import fabs
 n = int(input())
 for i in range(2 * n - 1):
- t = int(fabs(n - i - 1))
- print('-'.join([chr(97 + t + int(fabs(j - (n - t - 1) ))) for j in range(2 * (n - t) - 1)]).center(4 * n - 3, '-'))
+ t = int(abs(n - i - 1))
+ print('-'.join([chr(97 + t + int(abs(j - (n - t - 1) ))) for j in range(2 * (n - t) - 1)]).center(4 * n - 3, '-'))
